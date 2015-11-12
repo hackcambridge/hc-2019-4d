f93d86753f698d3a00b3bc499b8b311764073d8d
Christian Silver <christian@decode.org.nz>
2015-11-12 11:14:48 +0000

    Fix some asset URL generation

--- gulpfile.js
+++ gulpfile.js
@@ -65,11 +65,15 @@ gulp.task('assets', function () {
 gulp.task('rev', function () {
   var rev = new $.revAll();
 
-  gulp.src('assets/dist/**')
+  return gulp.src('assets/dist/**')
     .pipe(rev.revision())
     .pipe(gulp.dest('assets/dist'))
     .pipe(rev.manifestFile())
     .pipe(gulp.dest('assets/dist'));
+});
+
+gulp.task('wait', function (cb) {
+  setTimeout(cb, 2000);
 });
 
 gulp.task('watch', ['build'], function () {
@@ -79,9 +83,13 @@ gulp.task('watch', ['build'], function () {
 });
 
 gulp.task('build', function () {
-  var args = ['clean', ['assets', 'scripts', 'styles']];
+  var args = ['clean', 'assets', 'scripts', 'styles'];
+
   if (prod) {
+    // HACK: Waiting for a little bit means all of the assets actually get rev'd
+    args.push('wait');
     args.push('rev');
   }
+
   sequence.apply(null, args);
 });
