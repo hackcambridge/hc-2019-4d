02f6ec1a35e14d4ac520cc2a586004e5a67c609e
Christian Silver <christian@decode.org.nz>
2016-01-29 11:29:48 +0000

    Circumvent Heroku timeout

--- sockets.js
+++ sockets.js
@@ -7,6 +7,9 @@ var utils = require('./utils.js')
 module.exports = function (server) {
   io = socketio(server);
 
+  // Change heartbeat to 40s - within Heroku's 55s limit
+  io.set('heartbeat interval', 40000);
+
   var viewer = io.of('/view');
 
   viewer.on('connection', function (socket) {
