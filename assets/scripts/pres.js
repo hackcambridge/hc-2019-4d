4da0e97a7308007164807bc0a6e9e43b3a9f5387
Christian Silver <christian@decode.org.nz>
2016-01-29 11:38:03 +0000

    Add basic view and scaling

--- assets/scripts/pres.js
+++ assets/scripts/pres.js
@@ -1,10 +1,47 @@
 var socket = require('socket.io-client');
 var $ = require('jquery');
 
-module.exports = function () {
-  if ($('.event-viewer').length != 0) {
+function calculateSizes($el) {
+  return {
+    width: $el.width(),
+    height: $el.height(),
+    aspect: $el.width() / $el.height()
+  }
+}
+
+class Viewer {
+  constructor($container) {
+    this.$elements = {
+      container: $container,
+      viewer: $container.find('.event-viewer')
+    }
+
+    this.bounds = {
+      viewer: calculateSizes(this.$elements.viewer)
+    }
+
+    this.resize();
+
+    $(window).on('resize', this.resize.bind(this));
+  }
+
+  resize() {
+    this.bounds.container = calculateSizes(this.$elements.container);
+
+    var scaleThrough = 'width';
+
+    // Container is wider than viewer, scale by height
+    if (this.bounds.container.aspect > this.bounds.viewer.aspect) {
+      scaleThrough = 'height';
+    }
+
+    var scale = this.bounds.container[scaleThrough] / this.bounds.viewer[scaleThrough];
+    this.$elements.viewer.css('transform', `scale(${scale})`);
+  }
+
+  _initSockets() {
     var id = null;
-    var io = socket('/view');
+    var io = this.io = socket('/view');
 
     io.on('connect', function () {
       console.log('Connection Successful');
@@ -17,5 +54,11 @@ module.exports = function () {
         location.reload(true);
       }
     });
+  }
+}
+
+module.exports = function () {
+  if ($('.event-viewer').length != 0) {
+    var viewer = new Viewer($('.event-viewer-container'));
   }
 };
