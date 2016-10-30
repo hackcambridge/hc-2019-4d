539e8ec32ce91818e72812362c37a3b17799d0e5
Christian Silver <christian@decode.org.nz>
2016-10-30 17:11:03 +0000

    Add file upload support

--- src/js/server/routes/apply.js
+++ src/js/server/routes/apply.js
@@ -1,6 +1,24 @@
 const express = require('express');
-const createApplicationForm = require('js/shared/application-form');
+const { createApplicationForm, maxFieldSize } = require('js/shared/application-form');
 const renderForm = require('js/shared/render-form');
+const multer = require('multer');
+const crypto = require('crypto');
+
+const applyFormUpload = multer({
+  // storage: s3?
+  limits: {
+    fields: 20,
+    fieldSize: maxFieldSize,
+  },
+  fileFilter(req, file, callback) {
+    // At this stage, we know we are only uploading a CV in PDF. Only accept PDFs
+    if (file.mimetype === 'application/pdf') {
+      callback(null, true);
+    }
+
+    callback(null, false);
+  },
+});
 
 const applyRouter = new express.Router();
 
@@ -8,8 +26,11 @@ applyRouter.get('/', (req, res) => {
   renderApplyPageWithForm(res, createApplicationForm());
 });
 
-applyRouter.post('/', (req, res) => {
+applyRouter.post('/', applyFormUpload.single('cv'), (req, res) => {
   const form = createApplicationForm();
+
+  // HACK: Put all our fields in the same place by moving the file into req.body
+  req.body.cv = req.file;
 
   form.handle(req.body, {
     success: () => {
--- src/js/shared/application-form.js
+++ src/js/shared/application-form.js
@@ -1,4 +1,5 @@
 const { fields, validators, widgets, create: createForm } = require('forms');
+const { field: fileField, typeValidator: fileTypeValidator, sizeValidator: fileSizeValidator } = require('./file-field');
 
 function textareaField(label, maxlength) {
   return fields.string({
@@ -9,14 +10,34 @@ function textareaField(label, maxlength) {
     validators: [
       validators.maxlength(maxlength),
     ],
+    cssClasses,
   })
 }
 
-module.exports = function createApplicationForm() {
+exports.maxFieldSize = 1024 * 1024 * 2; // 2mb
+
+const cssClasses = {
+  error: [ 'form-error-message' ],
+  label: [ 'form-label' ],
+  field: [ 'form-row' ],
+};
+
+/**
+ * Create the object representation of our application form.
+ *
+ * To support client side validation in browsers that don't have sufficient APIs, there is
+ * an option to disable file validation.
+ */
+exports.createApplicationForm = function createApplicationForm(validateFile = true) {
   return createForm({
-    cv: fields.string({
+    cv: fileField({
       label: 'Upload your CV',
       required: validators.required('This field is requred'),
+      validators: validateFile ? [
+        fileTypeValidator('application/pdf', 'Please upload a PDF'),
+        fileSizeValidator(exports.maxFieldSize, 'Your CV must be no larger than 2mb'),
+      ] : [],
+      cssClasses,
     }),
     development: fields.array({
       label: 'Where do you see yourself fitting in the development process? Tick all that apply.',
@@ -28,6 +49,7 @@ module.exports = function createApplicationForm() {
         product_management: 'Product Management',
         none: 'I\'m not sure!',
       },
+      cssClasses,
     }),
     links: textareaField('Give us some links, so we can learn more about you', 1000),
     learn: textareaField('What do you want to learn from this event?', 500),
@@ -35,13 +57,16 @@ module.exports = function createApplicationForm() {
     accomplishment: textareaField('What is a recent accomplishment that you\'re proud of?', 500),
     team_apply: fields.boolean({
       label: 'Are you applying as part of a team?',
+      cssClasses,
     }),
     team_placement: fields.boolean({
       label: 'If not, would you like us to place you in a team?',
+      cssClasses,
     }),
     terms: fields.boolean({
       label: 'Accept our terms and conditions yo',
       required: validators.matchValue(() => true, 'You must accept our terms and conditions to apply'),
+      cssClasses,
     }),
   }, {
     validatePastFirstError: true,
