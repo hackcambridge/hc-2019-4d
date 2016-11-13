d441859bde2a50beebd4c62ab018c00916404695
Jared Khan <jaredkhan@me.com>
2016-11-13 15:43:44 +0000

    3 word memorable slugs with collision detection

--- src/js/server/apply/logic.js
+++ src/js/server/apply/logic.js
@@ -2,24 +2,25 @@ const crypto = require('crypto');
 const { HackerApplication } = require('js/server/models');
 const { sendEmail } = require('js/server/email');
 const emailTemplates = require('./email-templates');
+const generate = require("adjective-adjective-animal");
 
 exports.createApplicationFromForm = function (formData, user) {
-  const applicationSlug = crypto.randomBytes(64).toString('hex');
-
-  return HackerApplication.create({
-    // Foreign key
-    hackerId: user.id,
-    // Application
-    applicationSlug,
-    cv: formData.cv.location,
-    countryTravellingFrom: formData.countryTravellingFrom,
-    developmentRoles: formData.development,
-    learningGoal: formData.learn,
-    interests: formData.interests,
-    recentAccomplishment: formData.accomplishment,
-    links: formData.links,
-    inTeam: formData.team_apply,
-    wantsTeam: formData.team_placement,
+  return generate().then(slug => {
+    return HackerApplication.create({
+      // Foreign key
+      hackerId: user.id,
+      // Application
+      applicationSlug: slug,
+      cv: formData.cv.location,
+      countryTravellingFrom: formData.countryTravellingFrom,
+      developmentRoles: formData.development,
+      learningGoal: formData.learn,
+      interests: formData.interests,
+      recentAccomplishment: formData.accomplishment,
+      links: formData.links,
+      inTeam: formData.team_apply,
+      wantsTeam: formData.team_placement,
+    })
   }).then(application => {
     sendEmail({
       to: user.email, 
@@ -33,7 +34,14 @@ exports.createApplicationFromForm = function (formData, user) {
     console.log(`An application was successfully made by ${user.firstName} ${user.lastName}.`);
     return application;
   }).catch(err => {
-    console.log('Failed to add an application to the database');
-    return Promise.reject(err);
+    if (err.name == 'SequelizeUniqueConstraintError' && err.errors[0].path === 'applicationSlug') {
+      // slug was not unique, try again with new slug
+      console.log('Application slug collision detected');
+      return exports.createApplicationFromForm(formData, user);
+    } else {
+      console.log('Failed to add an application to the database');
+      return Promise.reject(err);
+    }
+    
   });
 };
