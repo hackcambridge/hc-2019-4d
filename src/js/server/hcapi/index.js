0bb4047dcf81d714823528c7e581ff30504b0753
Christian Silver <christian@decode.org.nz>
2016-12-07 00:26:52 +0000

    Add CORS to our API

--- src/js/server/hcapi/index.js
+++ src/js/server/hcapi/index.js
@@ -1,29 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const BearerStrategy = require('passport-http-bearer');
-const { Admin, OauthAccessToken } = require('js/server/models');
 const errors = require('./errors');
-
-passport.use(new BearerStrategy((token, done) => {
-  OauthAccessToken.getAdminFromTokenString(token)
-    .then((admin) => {
-      if (!admin) {
-        done(null, false);
-        return;
-      }
-
-      done(null, admin, { scope: 'all' });
-    })
-    .catch(error => done(error));
-}));
+const auth = require('./auth');
 
 /**
  * The hcapi is a separate express app to completely separate anything going on in our main website
  */
 const hcapi = express();
 
-hcapi.use(passport.authenticate('bearer', { session: false }));
+hcapi.options(auth.middleware.cors);
+hcapi.use(auth.middleware.cors);
+hcapi.use(auth.middleware.bearer);
 hcapi.use(bodyParser.json());
 
 // API endpoints
