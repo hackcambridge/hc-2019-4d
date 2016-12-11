7ccb7dcb370e54646d09e30958c8b19eef2771a6
Jared Khan <jaredkhan@me.com>
2016-12-11 02:53:49 +0000

    Add applications reviewed stat

--- src/js/server/hcapi/stats.js
+++ src/js/server/hcapi/stats.js
@@ -1,5 +1,6 @@
+const Sequelize = require('sequelize');
 const { Router } = require('express');
-const { Hacker, ApplicationReview, HackerApplication } = require('js/server/models');
+const { db, Hacker, ApplicationReview, HackerApplication } = require('js/server/models');
 const { createHttpError } = require('./errors');
 
 const statsRouter = new Router();
@@ -11,19 +12,46 @@ statsRouter.get('/', (req, res, next) => {
   // - The total number of sign ups
   // - The number of reviews (note that each application needs to be reviewed twice)
   // TODO: Get the numbers of reviews made per admin
+  // - The number of applications that have been reviewed at least twice
 
   const hackerCountPromise = Hacker.count();
   const hackerApplicationCountPromise = HackerApplication.count();
   const reviewCountPromise = ApplicationReview.count();
 
-  Promise.all([hackerCountPromise, hackerApplicationCountPromise, reviewCountPromise]).then((counts) => {
-    res.json({
-      hackerCount: counts[0],
-      hackerApplicationCount: counts[1],
-      reviewCount: counts[2],
-    });
-  }).catch(next);
+  const applicationsReviewedQuery = 
+    "SELECT COUNT(*) FROM (" + 
+      "SELECT \"hackerApplicationId\", COUNT(id)" + 
+      "FROM \"application-reviews\"" +
+      "GROUP BY \"hackerApplicationId\"" +
+    ") review_counts";
 
+  const applicationsReviewedCountPromise = 
+  db.query(applicationsReviewedQuery, { type: db.QueryTypes.SELECT }).then((counts) => {
+    // Get the number from the object that's returned
+    return parseInt(counts[0].count);
+  });
+
+  Promise.all([
+    hackerCountPromise,
+    hackerApplicationCountPromise,
+    reviewCountPromise,
+    applicationsReviewedCountPromise
+  ])
+  .then(
+    ([
+      hackerCount,
+      hackerApplicationCount,
+      reviewCount,
+      applicationsReviewedCount,
+    ]) => {
+      res.json({
+        hackerCount,
+        hackerApplicationCount,
+        reviewCount,
+        applicationsReviewedCount
+      });
+    }
+  ).catch(next);
 });
 
 module.exports = statsRouter;
