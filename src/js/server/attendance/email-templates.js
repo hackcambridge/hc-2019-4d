af339f21956ec3b6ba1465a4c85e9963d7c894be
Christian Silver <christian@decode.org.nz>
2016-12-31 14:49:51 +1300

    Update email copy for responses/tickets

--- src/js/server/attendance/email-templates.js
+++ src/js/server/attendance/email-templates.js
@@ -3,13 +3,21 @@ const { makeInstruction } = require('js/server/email');
 module.exports = {
   newTicket({ name }) {
     return {
-      subject: 'Here are your Hack Cambridge tickets',
+      subject: `${name}, here are your Hack Cambridge tickets`,
       body: {
         name,
         intro: [
-          'You\'ve been invited and RSVP\'d to Hack Cambridge. Here\'s your ticket!',
-          'Well, this email isn\'t your actual ticket. Blah blah.',
-          // TODO: Add info about registration
+          'You\'ve confirmed your place at Hack Cambridge. Here\'s your ticket!',
+          'Well, this email isn\'t your actual ticket, we will know who you are because you have to bring photo ID with you to registration.',
+        ],
+        action: [
+          makeInstruction({
+            instructions: 'All the information about registration, accomodation, travel and more is on your dashboard. Have a good read - there may be some extra steps for you.',
+            button: {
+              text: 'Go to my Dashboard',
+              link: 'https://hackcambridge.com/apply/dashboard',
+            },
+          })
         ],
         outro: 'If you have any questions or concerns, don\'t hesitate to reach out to us by visiting our website.',
       },
