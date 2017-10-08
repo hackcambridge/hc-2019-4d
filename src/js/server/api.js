2e0c1ee2b34cc0f2a3d4412d82bb112d1e98d1fa
Henry Mercer <henry.mercer@me.com>
2017-10-08 19:39:58 +0100

    Hook signup form on splash page up to subscribe/interested API endpoint

--- src/js/server/api.js
+++ src/js/server/api.js
@@ -3,18 +3,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('lodash');
 var stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
-var googleSpreadsheet = require("google-spreadsheet");
 
 const MC = new mailchimp.Mailchimp(process.env.MAILCHIMP_API_KEY);
-var google_sheets_auth_email = process.env.GOOGLE_SHEETS_AUTH_EMAIL;
-var google_sheets_auth_key = (process.env.GOOGLE_SHEETS_AUTH_KEY || '').replace(/\\n/g, '\n');
-var google_sheets_wifi_sheet_id = process.env.GOOGLE_SHEETS_WIFI_SHEET_ID;
 
 var api = module.exports = new express.Router();
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({ extended: true }));
 
-api.post('/subscribe', function (req, res, next) {
+api.post('/subscribe/interested', function (req, res, next) {
   if (_.isEmpty(req.body.email)) {
     var err = new Error('Must provide email');
     err.status = 401;
@@ -24,7 +20,7 @@ api.post('/subscribe', function (req, res, next) {
 
   // TODO: Check user is already subscribed
   MC.lists.subscribe({
-    id: process.env.MAILCHIMP_LIST_ID,
+    id: process.env.MAILCHIMP_INTERESTED_LIST_ID,
     email: { email: req.body.email },
     merge_vars: { EMAIL: req.body.email },
     update_existing: true
