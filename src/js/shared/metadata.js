9e0b343ec5ca650db6f212b49b891fb53c2810bf
Henry Mercer <henry.mercer@me.com>
2017-11-07 18:48:43 +0000

    Generate description programmatically from start and end dates

---
+++ src/js/shared/metadata.js
@@ -0,0 +1,15 @@
+const { getHackathonStartDate, getHackathonEndDate } = require('./dates');
+
+function createDescription() {
+  if (!getHackathonStartDate().isSame(getHackathonEndDate(), 'month')) {
+    throw new Error('Hackathon start and end dates must be in the same month to create description');
+  }
+
+  const formattedDateRange = getHackathonStartDate().format('MMMM D') + ' – ' + getHackathonEndDate().format('D');
+  
+  return `Join us for 24 hours of building, breaking and creating in the heart of Cambridge from ${formattedDateRange}.`;
+}
+
+module.exports.description = createDescription();
+
+module.exports.title = 'Hack Cambridge';
