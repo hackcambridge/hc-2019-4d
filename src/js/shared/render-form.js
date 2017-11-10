aea24cdac449be3c8e109a081db7448410ea5a37
Justin <j.malcic@me.com>
2017-11-10 02:36:13 +0000

    Removal of intermediate div

--- src/js/shared/render-form.js
+++ src/js/shared/render-form.js
@@ -9,14 +9,11 @@ function isMultipleField(type) {
 module.exports = function (name, field, options = { }) {
   const widgetHtml = field.widget.toHTML(name, field);
   const innerContent = isMultipleField(field.widget.type) ? tag('fieldset', { }, widgetHtml) : widgetHtml;
-  const column = tag('div', { classes: ['unit column'] }, [
-    field.note ? tag('p', { classes: ['form-control-note'] }, field.note) : '',
-    innerContent
-  ].join(''));
   const fieldSet = tag('fieldset', { classes: [ field.classes().join(' '), 'unit column'], required: !!field.required }, [
     tag('legend', { classes: ['form-control-legend'] }, field.label),
+    field.note ? tag('p', { classes: ['form-control-note'] }, field.note) : '',
     field.errorHTML(),
-    column,
+    innerContent,
   ].join(''));
   return tag('div', { classes: [field.row_units, 'unit row'] }, [
     fieldSet
