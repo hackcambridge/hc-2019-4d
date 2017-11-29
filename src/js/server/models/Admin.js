b5544857654f7b44276f7be1f39d5bf6afb5758d
Henry Mercer <henry.mercer@me.com>
2017-11-29 01:45:17 +0000

    Add low committal property to model

--- src/js/server/models/Admin.js
+++ src/js/server/models/Admin.js
@@ -11,6 +11,12 @@ module.exports = db.define('admin', {
     allowNull: false,
     unique: true,
   },
+  // If the admin is low committal, don't count them when calculating targets.
+  lowCommittal: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  }
 }, {
   tableName: 'admins',
 });
