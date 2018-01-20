1c1d6a0e44f151dbd1966ac9263ef8db2aed6c83
Henry Mercer <henry.mercer@me.com>
2018-01-20 22:42:21 +0000

    Add event info to live page

--- src/js/client/live.js
+++ src/js/client/live.js
@@ -2,6 +2,26 @@ const Countdown = require('../shared/countdown');
 
 const Pusher = require('pusher-js');
 const $ = require('jquery');
+const moment = require('moment');
+
+function eventNameList(events) {
+  return '<ul> ' + events.map(event => `<li>${event.name}</li>`).join(' ') + ' </ul>';
+}
+
+function refreshEventInfo() {
+  $.getJSON('/live-api/event-info', eventInfo => {
+    const currentEvents = eventInfo.currentEvents;
+    const nextEvents = eventInfo.nextEvents;
+
+    if (currentEvents.length > 0) {
+      $('.live-event-now').html(`${moment(currentEvents[0].time).format('HH:mm')} ${eventNameList(currentEvents)}`);
+    }
+
+    if (nextEvents.length > 0) {
+      $('.live-event-next').html(`${moment(nextEvents[0].time).format('HH:mm')} ${eventNameList(nextEvents)}`);
+    }
+  });
+}
 
 function initialiseLive() {
   const pusher = new Pusher(window.liveConfig.pusherKey, {
@@ -9,13 +29,16 @@ function initialiseLive() {
     cluster: 'eu',
   });
 
+  refreshEventInfo();
+  window.setInterval(refreshEventInfo, 10000);
+
   const liveUpdates = pusher.subscribe('live-updates');
 
   $('.live-social-feed-content').each(function () {
     liveUpdates.bind('social', (data) => {
-      $(this).children().remove();
-      $(this).append(data.statuses.slice(0, 5).map(status => {
-        const $socialItem = $('<div class="live-social-feed-item"><div class="live-social-feed-item-content"></div></div>');
+      $(this).children('.live-social-feed-item-twitter').remove();
+      $(this).append(data.statuses.slice(0, 4).map(status => {
+        const $socialItem = $('<div class="live-social-feed-item live-social-feed-item-twitter"><div class="live-social-feed-item-content"></div></div>');
         const $socialTitle = $('<h4></h4>');
         $socialTitle.text(status.username);
         const $socialContent = $('<p></p>');
