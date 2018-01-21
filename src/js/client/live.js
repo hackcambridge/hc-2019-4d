35d157b76d37e32b847adc7a6f36bcf652cc98e4
Justin Malčić <j.malcic@me.com>
2018-01-21 02:31:32 +0000

    Add final improvements

--- src/js/client/live.js
+++ src/js/client/live.js
@@ -1,24 +1,33 @@
 function generateFeedItem (status) {
-  const $socialItem = $('<div class="live-social-feed-item five unit row"><div class="live-social-feed-item-content unit column grows"></div></div>');
+  const $socialItem = $('<div class="live-social-feed-item unit row"><div class="live-social-feed-item-content unit column grows"><div class="five unit row social-image-container"><div class="unit column grows social-image"></div></div></div></div>');
   const $socialTitle = $('<h6></h6>');
-  $socialTitle.text(status.username);
+  $socialTitle.html(status.username);
   const $socialContent = $('<p></p>');
   const statusText = status.text.split(' ').slice(0, -1).join(' ');
   $socialContent.html(statusText);
   $socialItem.find('.live-social-feed-item-content').append($socialTitle).append($socialContent);
   if (status.image) {
-    $socialItem.find('.live-social-feed-item-content').css('background-image', `linear-gradient(to top, rgba(0, 0, 0, 0.6) 0, rgba(0, 0, 0, 0.2) 200px, rgba(0, 0, 0, 0)), url(${status.image})`);
+    $socialItem.find('.social-image').css('background-image', `url(${status.image})`).css('background-size', 'cover');
+  }
+  if (! status.image) {
+    $socialItem.find('.social-image-container').css('display', 'none');
   }
   return $socialItem;
 }
 
 function cycleFeedItems (statuses, element) {
-  for (const statusData in statuses) {
-    setTimeout(() => {
-      $(element).children().remove();
-      $(element).append(generateFeedItem(statuses[statusData]));
-    }, 5000);
-  }
+  var i = 0;
+  $(element).append(generateFeedItem(statuses[i]));
+  i++;
+  setInterval(() => {
+    $(element).children().remove();
+    $(element).append(generateFeedItem(statuses[i]));
+    if (i == statuses.length - 1) {
+      i = 0;
+    } else {
+      i++;
+    }
+  }, 5000);
 }
 
 const Countdown = require('../shared/countdown');
@@ -28,7 +37,7 @@ const $ = require('jquery');
 const moment = require('moment');
 
 function eventNameList(events) {
-  return '<ul> ' + events.map(event => `<li>${event.name}</li>`).join(' ') + ' </ul>';
+  return events.map(event => `${event.name}`).join(' ');
 }
 
 function refreshEventInfo() {
@@ -37,11 +46,13 @@ function refreshEventInfo() {
     const nextEvents = eventInfo.nextEvents;
 
     if (currentEvents.length > 0) {
-      $('.live-event-now').html(`${moment(currentEvents[0].time).format('HH:mm')} ${eventNameList(currentEvents)}`);
+      $('.live-event-now-time').html(`${moment(currentEvents[0].time).format('HH:mm')}`);
+      $('.live-event-now-text').html(`${eventNameList(currentEvents)}`);
     }
 
     if (nextEvents.length > 0) {
-      $('.live-event-next').html(`${moment(nextEvents[0].time).format('HH:mm')} ${eventNameList(nextEvents)}`);
+      $('.live-event-next-time').html(`${moment(nextEvents[0].time).format('HH:mm')}`);
+      $('.live-event-next-text').html(`${eventNameList(nextEvents)}`);
     }
   });
 }
