909d8a49f08177668de29ad8bb1cf7331d99b16b
Justin Malčić <j.malcic@me.com>
2018-01-21 08:21:27 +0000

    Fix flashing

--- gulpfile.js
+++ gulpfile.js
@@ -53,6 +53,17 @@ gulp.task('styles', () => {
     .pipe($.if(!prod, $.sourcemaps.write()))
     .pipe(gulp.dest('assets/dist/styles/hc-2018'))
     .pipe(bs.stream());
+  gulp.src('src/styles/hc-2018/ternary-cube.styl')
+    .pipe($.if(!prod, $.sourcemaps.init()))
+    .pipe($.stylus({
+      'include css': true,
+      paths: ['./node_modules'],
+
+    }))
+    .pipe($.autoprefixer())
+    .pipe($.if(!prod, $.sourcemaps.write()))
+    .pipe(gulp.dest('assets/dist/styles/hc-2018'))
+    .pipe(bs.stream());
 });
 
 // js
