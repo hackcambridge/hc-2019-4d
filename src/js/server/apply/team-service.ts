e72b8c247d7c804e5f01db4ea446815656d984c1
Henry Mercer <henry.mercer@me.com>
2018-11-04 15:49:37 +0000

    Add team service with conversions to wants team and applying on own

---
+++ src/js/server/apply/team-service.ts
@@ -0,0 +1,66 @@
+import { HackerInstance } from "../models/Hacker";
+import { TeamInstance } from "../models/Team";
+
+interface TeamServiceConfig {
+  sendUserLeftTeamEmail(emailRecipient: string, emailLeaving: string);
+}
+
+interface TeamServiceInterface {
+  /**
+   * Takes a hacker in an arbitrary state and converts them to the "wants to be allocated to a team" state.
+   */
+  convertHackerToWantsTeam(hacker: HackerInstance): Promise<void>;
+
+  /**
+   * Takes a hacker in an arbitrary state and converts them to the "applying on their own" state.
+   */
+  convertHackerToApplyingOnOwn(hacker: HackerInstance): Promise<void>;
+
+  /**
+   * Takes a hacker and removes them from their team, notifying any other members of the team and
+   * removing the team if there are no remaining members.
+   */
+  leaveTeam(hacker: HackerInstance): Promise<void>;
+}
+
+class TeamService implements TeamServiceInterface {
+  /**
+   * Takes a hacker who has applied and converts them to the "wants to be allocated to a team" state.
+   */
+  async convertHackerToWantsTeam(hacker: HackerInstance): Promise<void> {
+    if (await hacker.getTeam() !== null) {
+      await this.leaveTeam(hacker);
+    }
+    const application = await hacker.getHackerApplication();
+    if (application === null) {
+      throw new Error('TeamService.convertHackerToWantsTeam: expecting hacker to have applied.');
+    }
+    application.inTeam = false;
+    application.wantsTeam = true;
+    await application.save();
+  }
+
+  /**
+   * Takes a hacker who has applied and converts them to the "applying on their own" state.
+   */
+  async convertHackerToApplyingOnOwn(hacker: HackerInstance): Promise<void> {
+    if (await hacker.getTeam() !== null) {
+      await this.leaveTeam(hacker);
+    }
+    const application = await hacker.getHackerApplication();
+    if (application === null) {
+      throw new Error('TeamService.convertHackerToWantsTeam: expecting hacker to have applied.');
+    }
+    application.inTeam = false;
+    application.wantsTeam = false;
+    await application.save();
+  }
+
+  /**
+   * Takes a hacker and removes them from their team, notifying any other members of the team and
+   * removing the team if there are no remaining members.
+   */
+  leaveTeam(hacker: HackerInstance): Promise<void> {
+    throw new Error('unimplemented');
+  }
+}
--- src/js/server/models/Team.ts
+++ src/js/server/models/Team.ts
@@ -6,8 +6,23 @@ import { TeamMemberInstance } from './TeamMember';
 export interface TeamInstance extends Sequelize.Instance<{}> {
   id?: number;
   teamMembers: TeamMemberInstance[];
+
+  /**
+   * Returns the total number of members, including invitees.
+   */
+  getTotalMembersCount?: () => number;
 }
 
-export default db.define<TeamInstance, {}>('team', { }, {
-  tableName: 'teams'
+/**
+ * Returns the total number of members, including invitees.
+ */
+function getTotalMembersCount(this: TeamInstance) {
+  return this.teamMembers.length;
+}
+
+const Team = db.define<TeamInstance, {}>('team', { }, {
+  tableName: 'teams',
+  instanceMethods: [getTotalMembersCount]
 });
+
+export default Team;
