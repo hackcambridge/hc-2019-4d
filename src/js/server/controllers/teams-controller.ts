433eace5da1954a7ef9cade6f8e57f96e341e199
Henry Mercer <henry.mercer@me.com>
2018-11-10 15:48:54 +0000

    Refactor teams controller

--- src/js/server/controllers/teams-controller.ts
+++ src/js/server/controllers/teams-controller.ts
@@ -1,9 +1,9 @@
-import { checkSchema, validationResult, ValidationSchema } from 'express-validator/check';
+import { checkSchema, validationResult, ValidationSchema, Result } from 'express-validator/check';
 
 import { UserRequest } from 'js/server/apply/router';
-import { HackerApplication, ApplicationResponse, Team, TeamMember } from 'js/server/models';
-import { sendEmail } from 'js/server/email';
-import * as emailTemplates from 'js/server/apply/email-templates';
+import { HackerApplication, ApplicationResponse, Team, TeamMember, HackerInstance } from 'js/server/models';
+import { TeamMemberInstance } from '../models/TeamMember';
+import { RequestHandlerParams } from '../../../../node_modules/@types/express-serve-static-core';
 
 const schema: ValidationSchema = {
   'members.b': {
@@ -29,23 +29,21 @@ const schema: ValidationSchema = {
   },
 }
 
-export function newTeam(req: UserRequest, res) {
-  req.user.getHackerApplication().then(hackerApplication => {
-    if (hackerApplication !== null) {
-      req.user.getTeam().then(team => {
-        if (team === null) {
-          res.render('apply/team.html', { applicationSlug: hackerApplication.applicationSlug });
-        } else if (team !== null) {
-          res.redirect('/apply/dashboard');
-        }
-      });
-    } else if (hackerApplication === null)  {
-      res.redirect('/apply/form');
+export async function newTeam(req: UserRequest, res): Promise<void> {
+  const hackerApplication = await req.user.getHackerApplication();
+  if (hackerApplication !== null) {
+    const team = await req.user.getTeam();
+    if (team === null) {
+      res.render('apply/team.html', { applicationSlug: hackerApplication.applicationSlug });
+    } else if (team !== null) {
+      res.redirect('/apply/dashboard');
     }
-  });
+  } else if (hackerApplication === null)  {
+    res.redirect('/apply/form');
+  }
 }
 
-export const createTeam = [
+export const createTeam: RequestHandlerParams[] = [
   checkSchema(schema),
   (req: UserRequest, res, next) => {
     const errors = validationResult(req);
@@ -65,10 +63,10 @@ export const createTeam = [
       });
     }
   }
-]
+];
 
-export function createTeamFromForm(body, user, errors) {
-  const members = new Set();
+export async function createTeamFromForm(body, user: HackerInstance, errors: Result<{}>): Promise<TeamMemberInstance[]> {
+  const members = new Set<string>();
   const hackerIds = [user.id];
   const applicationSlugs = {
     'memberB': body.members.b,
@@ -76,70 +74,67 @@ export function createTeamFromForm(body, user, errors) {
     'memberD': body.members.d,
   };
   // Ensure application slugs are unique and not the applicant's own
-  return user.getHackerApplication().then(application => {
-    // Start off with the current hacker's application slug — we already know they're in the team
-    members.add(application.applicationSlug);
-    return new Promise((resolve, reject) => {
-      for (const field in applicationSlugs) {
-        applicationSlugs[field] = applicationSlugs[field].trim();
-        if (applicationSlugs[field] !== '') {
-          if (!members.has(applicationSlugs[field])) {
-            members.add(applicationSlugs[field]);
-          } else {
-            throw new Error(errors[field] = 'Each application ID must be distinct.');
-          }
-        } else {
-          delete applicationSlugs[field];
-        }
+  const application = await user.getHackerApplication();
+  // Start off with the current hacker's application slug — we already know they're in the team
+  members.add(application.applicationSlug);
+
+  for (const field in applicationSlugs) {
+    applicationSlugs[field] = applicationSlugs[field].trim();
+    if (applicationSlugs[field] !== '') {
+      if (!members.has(applicationSlugs[field])) {
+        members.add(applicationSlugs[field]);
+      } else {
+        throw new Error(errors[field] = 'Each application ID must be distinct.');
       }
-      resolve();
-    });
-  }).then(() => {
-    if (members.size > 1) {
-      return Promise.all(Object.keys(applicationSlugs).map(field => {
-        const applicationSlug = applicationSlugs[field];
-        return HackerApplication.findOne({
-          where: { applicationSlug }
-        }).then(application => {
-          if (application === null) {
-            // The application slug was not valid
-            throw new Error(errors[field] = 'There aren\'t any applications with this ID!');
-          }
-          return application;
-        }).then(application => {
-          hackerIds.push(application.hackerId);
-          return TeamMember.findOne({
-            where: { hackerId: application.hackerId }
-          }).then(team => {
-            if (team !== null) {
-              // The hacker is already part of another team
-              throw new Error(errors[field] = 'This applicant is already part of a different team!');
-            }
-            return ApplicationResponse.findOne({
-              where: { hackerApplicationId: application.id }
-            });
-          }).then(applicationResponse => {
-            if (applicationResponse !== null) {
-              // The hacker has already been either accepted or rejected
-              throw new Error(errors[field] = 'This applicant has already been reviewed.');
-            }
-            return applicationResponse;
-          });
-        });
-      }));
     } else {
-      throw new Error(errors['memberB'] = 'You need at least two team members to form a team.');
+      delete applicationSlugs[field];
     }
-  }).then(() => {
-    // Create a new team
-    return Team.create({ }).then(team => {
-      // Add the team members to the team
-      return Promise.all(hackerIds.map(hackerId => {
-        return TeamMember.create({
-          teamId: team.id,
-          hackerId
-        });
-      }));
+  }
+
+  if (members.size <= 1) {
+    throw new Error(errors['memberB'] = 'You need at least two team members to form a team.');
+  }
+
+  await Promise.all(Object.keys(applicationSlugs).map(async field => {
+    const applicationSlug = applicationSlugs[field];
+
+    const application = await HackerApplication.findOne({
+      where: { applicationSlug }
     });
-  });
+
+    if (application === null) {
+      // The application slug was not valid
+      throw new Error(errors[field] = 'There aren\'t any applications with this ID!');
+    }
+    hackerIds.push(application.hackerId);
+
+    const team = await TeamMember.findOne({
+      where: { hackerId: application.hackerId }
+    });
+
+    if (team !== null) {
+      // The hacker is already part of another team
+      throw new Error(errors[field] = 'This applicant is already part of a different team!');
+    }
+
+    const applicationResponse = await ApplicationResponse.findOne({
+      where: { hackerApplicationId: application.id }
+    });
+
+    if (applicationResponse !== null) {
+      // The hacker has already been either accepted or rejected
+      throw new Error(errors[field] = 'This applicant has already been reviewed.');
+    }
+  }));
+
+  // Create a new team
+  const team = await Team.create({ });
+
+  // Add the team members to the team
+  return Promise.all(hackerIds.map(hackerId =>
+    TeamMember.create({
+      teamId: team.id,
+      hackerId
+    })
+  ));
 }
