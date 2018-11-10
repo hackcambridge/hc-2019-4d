15f88ce602b1babebf0e19cd1e3600ef005a6a95
Henry Mercer <henry.mercer@me.com>
2018-11-10 15:35:17 +0000

    Refactor dashboard controller

--- src/js/server/controllers/dashboard-controller.ts
+++ src/js/server/controllers/dashboard-controller.ts
@@ -1,89 +1,80 @@
 import { UserRequest } from 'js/server/apply/router';
 import * as utils from '../utils.js';
-import { Hacker, TeamMember } from 'js/server/models';
+import { Hacker, TeamMember, HackerInstance } from 'js/server/models';
 import * as statuses from 'js/shared/status-constants';
 import { getHackathonStartDate, getHackathonEndDate } from 'js/shared/dates';
+import { RequestHandler } from '../../../../node_modules/@types/express';
 
-export function showDashboard(req: UserRequest, res) {
-  const content = utils.loadResource('dashboard');
-  let application;
-  let applicationStatus;
-
-  req.user.getHackerApplication().then(hackerApplication => {
-    application       = hackerApplication;
-    applicationStatus = req.user.getApplicationStatus(hackerApplication);
-
-    const teamApplicationStatusPromise    = req.user.getTeamApplicationStatus(hackerApplication);
-    const responseStatusPromise           = req.user.getResponseStatus(hackerApplication);
-    const rsvpStatusPromise               = req.user.getRsvpStatus(hackerApplication);
-    const ticketStatusPromise             = req.user.getTicketStatus(hackerApplication);
-
-    const teamMembersPromise = req.user.getTeam().then(teamMember => {
-      if (teamMember === null) {
-        return null;
-      } else {
-        const teamId = teamMember.teamId;
-        return TeamMember.findAll({
-          where: <any>{
-            teamId: teamId,
-            $not: {
-              // Exclude the current user
-              hackerId: req.user.id,
-            }
-          },
-        });
+async function getOtherTeamMembersAsHackers(user: HackerInstance): Promise<HackerInstance[]> {
+  const teamMember = await user.getTeam();
+  if (teamMember == null) {
+    return null;
+  }
+  const otherMembers = await TeamMember.findAll({
+    where: <any>{
+      teamId: teamMember.teamId,
+      $not: {
+        // Exclude the current user
+        hackerId: user.id,
       }
-    }).then(teamMembers => {
-      if (teamMembers == null) {
-        return null;
-      }
-      return Promise.all(
-        teamMembers.map(member => member.getHacker())
-      );
-    });
-
-    return Promise.all([
-      teamApplicationStatusPromise,
-      responseStatusPromise,
-      rsvpStatusPromise,
-      teamMembersPromise,
-      ticketStatusPromise,
-    ]);
-  }).then(([teamApplicationStatus, responseStatus, rsvpStatus, teamMembers, ticketStatus]) => {
-    const overallStatus = Hacker.deriveOverallStatus(
-      applicationStatus,
-      responseStatus,
-      teamApplicationStatus,
-      rsvpStatus,
-      ticketStatus
-    );
-
-    const fridayWeekday = 5;
-    const fridayBeforeHackathonDate = (getHackathonStartDate().isoWeekday() > fridayWeekday) ? getHackathonStartDate().isoWeekday(fridayWeekday) : getHackathonStartDate().subtract(1, 'week').isoWeekday(fridayWeekday);
-
-    res.render('apply/dashboard.html', {
-      applicationSlug: (application === null) ? null : application.applicationSlug,
-      applicationStatus,
-      wantsTeam: (application === null) ? null : application.wantsTeam,
-      teamApplicationStatus,
-      responseStatus,
-      rsvpStatus,
-      ticketStatus,
-      overallStatus,
-
-      applicationInfo: content['your-application'][applicationStatus],
-      teamApplicationInfo: content['team-application'][teamApplicationStatus],
-      rsvpInfo: content['rsvp'][rsvpStatus],
-      statusMessage: content['status-messages'][overallStatus],
-      teamMembers,
-
-      applicationsOpenStatus: process.env.APPLICATIONS_OPEN_STATUS,
-
-      hackathonStartDate: getHackathonStartDate().format('dddd DDDo MMM YYYY'),
-      hackathonEndDate: getHackathonEndDate().format('dddd DDDo MMM'),
-      fridayBeforeHackathonDate: fridayBeforeHackathonDate.format('DDDo MMM'),
-
-      statuses,
-    });
+    },
   });
+  if (otherMembers == null) {
+    return null;
+  }
+  return Promise.all(otherMembers.map(member => member.getHacker()));
 }
+
+export const showDashboard: RequestHandler = async (req: UserRequest, res) => {
+  const content = utils.loadResource('dashboard');
+
+  const application = await req.user.getHackerApplication();
+  const applicationStatus = req.user.getApplicationStatus(application);
+
+  const [teamApplicationStatus, responseStatus, rsvpStatus, ticketStatus, teamMembers] = await Promise.all([
+    req.user.getTeamApplicationStatus(application),
+    req.user.getResponseStatus(application),
+    req.user.getRsvpStatus(application),
+    req.user.getTicketStatus(application),
+    getOtherTeamMembersAsHackers(req.user)
+  ]);
+
+  const overallStatus = Hacker.deriveOverallStatus(
+    applicationStatus,
+    responseStatus,
+    teamApplicationStatus,
+    rsvpStatus,
+    ticketStatus
+  );
+
+  const fridayWeekday = 5;
+  const fridayBeforeHackathonDate =
+    (getHackathonStartDate().isoWeekday() > fridayWeekday)
+      ? getHackathonStartDate().isoWeekday(fridayWeekday)
+      : getHackathonStartDate().subtract(1, 'week').isoWeekday(fridayWeekday);
+
+  res.render('apply/dashboard.html', {
+    applicationSlug: (application === null) ? null : application.applicationSlug,
+    applicationStatus,
+    wantsTeam: (application === null) ? null : application.wantsTeam,
+    teamApplicationStatus,
+    responseStatus,
+    rsvpStatus,
+    ticketStatus,
+    overallStatus,
+
+    applicationInfo: content['your-application'][applicationStatus],
+    teamApplicationInfo: content['team-application'][teamApplicationStatus],
+    rsvpInfo: content['rsvp'][rsvpStatus],
+    statusMessage: content['status-messages'][overallStatus],
+    teamMembers,
+
+    applicationsOpenStatus: process.env.APPLICATIONS_OPEN_STATUS,
+
+    hackathonStartDate: getHackathonStartDate().format('dddd DDDo MMM YYYY'),
+    hackathonEndDate: getHackathonEndDate().format('dddd DDDo MMM'),
+    fridayBeforeHackathonDate: fridayBeforeHackathonDate.format('DDDo MMM'),
+
+    statuses
+  });
+};
