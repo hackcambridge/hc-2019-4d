16a8f828e29a3ba793c267f6656725e80a02eb79
Justin Malčić <j.malcic@me.com>
2018-11-16 23:45:41 +0000

    Update to match new functions

--- src/js/server/apply/applicant-info.ts
+++ src/js/server/apply/applicant-info.ts
@@ -8,21 +8,19 @@ export function getHackersWithUnfinishedApplications(kind) {
   return Hacker.findAll().then(hackers =>
     Promise.all(hackers.map(hacker =>
       hacker.getHackerApplication().then(hackerApplication =>
-        Promise.all([
-          getApplicationStatus(hackerApplication),
-          getTeamApplicationStatus(hackerApplication)
-        ]).then(([applicationStatus, teamApplicationStatus]) => {
-          if (kind == unfinishedApplicationKind.INDIVIDUAL) {
-            return applicationStatus == statuses.application.INCOMPLETE ? hacker : null;
-          } else if (kind == unfinishedApplicationKind.TEAM_ONLY) {
-            // The value of teamApplicationStatus is null if the individual application hasn't been finished,
-            // so ensure we only return the hacker when teamApplicationStatus is INCOMPLETE.
-            return teamApplicationStatus == statuses.teamApplication.INCOMPLETE ? hacker : null;
-          } else {
-            throw Error('Unknown unfinished application kind');
-          }
-        })
-      )
-    )).then(hackerResults => hackerResults.filter(result => result !== null))
+        Promise.all([getApplicationStatus(),getTeamApplicationStatus()])
+          .then(([applicationStatus, teamApplicationStatus]) => {
+            if (kind == unfinishedApplicationKind.INDIVIDUAL) {
+              return applicationStatus == statuses.application.INCOMPLETE ? hacker : null;
+            } else if (kind == unfinishedApplicationKind.TEAM_ONLY) {
+              // The value of teamApplicationStatus is null if the individual application hasn't been finished,
+              // so ensure we only return the hacker when teamApplicationStatus is INCOMPLETE.
+              return teamApplicationStatus == statuses.teamApplication.INCOMPLETE ? hacker : null;
+            } else {
+              throw Error('Unknown unfinished application kind');
+            }
+          })
+        )
+      )).then(hackerResults => hackerResults.filter(result => result !== null))
   );
 }
