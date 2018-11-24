d1a89cf16ab5dadf9ddcc9cb5db8dceb09ef3338
Justin Malčić <j.malcic@me.com>
2018-11-24 18:45:02 +0000

    Support all HTTP methods

--- src/client/main.ts
+++ src/client/main.ts
@@ -13,6 +13,20 @@ $(document).ready(() => {
   pages.forEach(f => f.start());
 });
 
+document.addEventListener('DOMContentLoaded', _ => {
+  const buttonsWithMethod = Array.from(document.getElementsByTagName('button')).filter(element =>
+    element.attributes.getNamedItem('data-method') !== null
+  );
+  buttonsWithMethod.forEach(element => {
+    element.addEventListener('click', event => {
+      event.preventDefault();
+      fetch(element.attributes.getNamedItem('data-action').value, {
+        method: element.attributes.getNamedItem('data-method').value
+      }).then(response => location.assign(response.url));
+    });
+  });
+});
+
 const application = Application.start();
 application.register('field-validator', FieldValidatorController);
 application.register('validation-messages', ValidationMessagesController);
