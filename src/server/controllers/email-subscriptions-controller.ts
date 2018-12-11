af4414aa1c6a7b9b3fc9a8a85c65ba9f7f9a8b78
Justin Malčić <j.malcic@me.com>
2018-12-11 04:51:27 +0000

    Add new controller

---
+++ src/server/controllers/email-subscriptions-controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import * as mailchimp from 'mailchimp-api';
+
+const MC = new mailchimp.Mailchimp(process.env.MAILCHIMP_API_KEY);
+
+export function createSubscription(req: Request, res: Response) {
+  // TODO: Check user is already subscribed
+  MC.lists.subscribe({
+    id: process.env.MAILCHIMP_INTERESTED_LIST_ID,
+    email: { email: req.body.email },
+    merge_vars: { EMAIL: req.body.email },
+    update_existing: true
+  },
+  _ => res.json('An email requesting your confirmation has been sent to the address you provided.'),
+  err => err.name === 'ValidationError' ? res.status(400).json(err.error) : res.status(500).json(err.error));
+}
