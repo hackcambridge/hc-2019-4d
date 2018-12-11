6cf6c273bdb22f778ce8d3a7b9369056df5c3146
Justin Malčić <j.malcic@me.com>
2018-12-11 11:05:28 +0000

    Add type annotation

--- src/client/controllers/email-sign-up-controller.ts
+++ src/client/controllers/email-sign-up-controller.ts
@@ -2,12 +2,12 @@ import { Controller } from 'stimulus';
 import { UJSEvent } from '../main';
 
 export default class EmailSignUpController extends Controller {
-  public processResponse(event: UJSEvent<any>) {
+  public processResponse(event: UJSEvent<string>) {
     const [data] = event.detail;
     alert(data);
   }
 
-  public handleError(event: UJSEvent<any>) {
+  public handleError(event: UJSEvent<string>) {
     const [ response, status ] = event.detail;
     alert(`The server returned a ${status} error.\nThe error was: ${response}`);
     console.log(response);
