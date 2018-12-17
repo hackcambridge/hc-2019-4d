553c5fd71ba367de4adfb0644cbdec21278188b0
Justin Malčić <j.malcic@me.com>
2018-12-17 00:25:22 +0000

    Add target

--- src/client/controllers/email-sign-up-controller.ts
+++ src/client/controllers/email-sign-up-controller.ts
@@ -2,6 +2,15 @@ import { Controller } from 'stimulus';
 import { UJSEvent } from '../types';
 
 export default class EmailSignUpController extends Controller {
+  /**
+   * Field
+   */
+  public fieldTarget: HTMLInputElement;
+  
+  static get targets() {
+    return ['field'];
+  }
+
   public processResponse(event: UJSEvent<string>) {
     const [data] = event.detail;
     alert(data);
