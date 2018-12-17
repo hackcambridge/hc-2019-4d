7c559944fd690f9161424ec73f90bf6e92378734
Justin Malčić <j.malcic@me.com>
2018-12-17 00:26:58 +0000

    Handle validation

--- src/client/controllers/email-sign-up-controller.ts
+++ src/client/controllers/email-sign-up-controller.ts
@@ -12,12 +12,16 @@ export default class EmailSignUpController extends Controller {
   }
 
   public processResponse(event: UJSEvent<string>) {
-    const [data] = event.detail;
-    alert(data);
+    const [ response ] = event.detail;
+    alert(response);
   }
 
   public handleError(event: UJSEvent<string>) {
     const [ response, status ] = event.detail;
-    alert(`The server returned a ${status} error.\nThe error was: ${response}`);
+    if (status === 'Bad Request') {
+      this.fieldTarget.setCustomValidity(response);
+    } else {
+      alert(`The server returned a ${status} error.\nThe error was: ${response}`);
+    }
   }
 }
