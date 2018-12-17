b503f7ef61a3dfe3cd992ecf6cfd4fea3a866d0a
Justin Malčić <j.malcic@me.com>
2018-12-17 00:26:13 +0000

    Refactor

--- src/server/controllers/email-subscriptions-controller.ts
+++ src/server/controllers/email-subscriptions-controller.ts
@@ -1,15 +1,9 @@
 import { Request, Response } from 'express';
-import * as mailchimp from 'mailchimp-api';
+import { Mailchimp } from 'mailchimp-api';
 
-const MC = new mailchimp.Mailchimp(process.env.MAILCHIMP_API_KEY);
+const MC = new Mailchimp(process.env.MAILCHIMP_API_KEY);
 
 export function createSubscription(req: Request, res: Response) {
-  // TODO: Check user is already subscribed
-  MC.lists.subscribe({
-    id: process.env.MAILCHIMP_INTERESTED_LIST_ID,
-    email: { email: req.body.email },
-    merge_vars: { EMAIL: req.body.email },
-    update_existing: true
   },
   _ => res.json('An email requesting your confirmation has been sent to the address you provided.'),
   err => {
@@ -17,6 +11,12 @@ export function createSubscription(req: Request, res: Response) {
       res.status(400).json(err.error);
     } else {
       res.status(500).json('The server encountered an error.');
+      // TODO: Check user is already subscribed
+      MC.lists.subscribe({
+        id: process.env.MAILCHIMP_INTERESTED_LIST_ID,
+        email: { email: req.body.email },
+        merge_vars: { EMAIL: req.body.email },
+        update_existing: true
     }
   });
 }
