56d19236eeace4e842cab3fc19c1c6c00b7a87c0
Justin Malčić <j.malcic@me.com>
2019-01-06 12:33:23 +0000

    Add type annotations

--- src/server/routes/event-router.ts
+++ src/server/routes/event-router.ts
@@ -1,12 +1,12 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 
 import { loadResource } from 'server/utils';
 
 const eventRouter = Router();
 
-eventRouter.get('/', (_req, res) => res.render('event/index'));
+eventRouter.get('/', (_req: Request, res: Response) => res.render('event/index'));
 
-eventRouter.get('/schedule', (_req, res) => {
+eventRouter.get('/schedule', (_req: Request, res: Response) => {
   res.render('event/schedule', {
     schedule: loadResource('schedule'),
     workshops: loadResource('workshops'),
@@ -14,13 +14,13 @@ eventRouter.get('/schedule', (_req, res) => {
   });
 });
 
-eventRouter.get('/hacking', (_req, res) => {
+eventRouter.get('/hacking', (_req: Request, res: Response) => {
   res.render('event/hacking', {
     apis: loadResource('apis'),
     prizes: loadResource('prizes')
   });
 });
 
-eventRouter.get('/location', (_req, res) => res.render('event/location'));
+eventRouter.get('/location', (_req: Request, res: Response) => res.render('event/location'));
 
 export default eventRouter;
